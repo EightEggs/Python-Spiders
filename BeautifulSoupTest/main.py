ce33d562cd06254c1afaff00dddd811c846c3a31
Eight_Eggs <906324036@qq.com>
2022-03-20 15:41:26 +0800

    Update PyQueryTest and BeautifulSoupTest.

--- BeautifulSoupTest/main.py
+++ BeautifulSoupTest/main.py
@@ -1,37 +1,85 @@
-import logging
-import requests
 from bs4 import BeautifulSoup
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s: %(message)s')
+html = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>The Dormous's story</title>
+</head>
+<body>
+  <p class="title" name="dormous"><b>The Dormous's story</b></p>
+  <p class="story">One upon a time there were three little sisters; and their names were
+  <a href="http://example.com/elsie" class="sister" id="link1"><span>Elsie</span></a>,
+  <a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+  <a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+  and they lived at the bottom of a well.
+  </p>
+  <p class="story">...</p>
+</body>
+</html>
+"""
+
+### Basic usage ###
+soup = BeautifulSoup(html,'lxml')
+print(soup.prettify())
+print(soup.title.string)
 
 
-def scrape_page(url):
-    logging.info('scraping %s...\n', url)
-    try:
-        response = requests.get(url=url)
-        if response.status_code == 200:
-            response.encoding = 'utf-8'
-            return response.text
-        logging.error('get invalid status code %s while scraping %s\n',
-                      response.status_code, url)
-    except requests.RequestException:
-        logging.error('error occurred while scraping %s\n', url, exc_info=True)
+### Nodes selector ###
+print(soup.head)
+print(soup.p) # only the first p element can be selected.
 
 
-def save_data(data):
-    """
-    save to json file
-    :param data:
-    :return:
-    """
-    data_path = 'result.html'
-    with open(data_path, 'w', encoding='utf-8') as f:
-        f.write(data)
+### Extract information ###
+# 1. name
+print(soup.title.name)
+
+# 2. attributes
+print(soup.p.attrs)
+print(soup.p.attrs['name']) # == print(soup.p['name'])
+
+# 3. contents
+print(soup.p.string)      # str
+print(soup.p.text)        # str
+print(soup.p.get_text())  # str
+print(soup.p.contents)    # list
 
 
-if __name__ == '__main__':
-    url = 'https://www.gitee.com'
-    html = scrape_page(url)
-    soup = BeautifulSoup(html, 'lxml')
-    save_data(soup.prettify(formatter='html'))
+### Relation selector ###
+# 1. children and descendants
+print(soup.body.children)
+for i,child in enumerate(soup.body.children):
+    print(i, child)
+print(soup.body.descendants)
+for i,child in enumerate(soup.body.descendants):
+    print(i, child)
+
+# 2. parent and parents
+print(soup.a.parent) # only the first a element can be selected.
+print(soup.a.parents)
+for i,parent in enumerate(soup.a.parents):
+    print(i, parent)
+
+# 3. siblings
+print("Next sibling:", soup.a.next_sibling)
+print("Prev sibling:", soup.a.previous_sibling)
+print("Next siblings:", list(soup.a.next_siblings))
+print("Prev siblings:", list(soup.a.previous_siblings))
+
+
+### Method selector ###
+"""
+find(), find_all(),
+find_parent(), find_parents(),
+find_next_sibling(), find_next_siblings(),
+find_previous_sibling(), find_previous_siblings(),
+find_next(), find_all_next(),
+find_previous(), find_all_previous()
+...
+"""
+
+
+### CSS selector ###
+print(soup.select('.title'))
+print(soup.select('p a'))
+print(soup.select_one('p a'))
--- PyQueryTest/main.py
+++ PyQueryTest/main.py
@@ -3,13 +3,13 @@ from pyquery import PyQuery as pq
 
 # 1. HTML string
 html = """
-<div>
-  <ul>
-    <li class="item-0"></li>
-    <li class="item-1"></li>
-    <li class="item-0"></li>
-    <li class="item-1"></li>
-    <li class="item-0"></li>
+<div id="container">
+  <ul class="mylist">
+    <li class="item-0">text1</li>
+    <li class="item-1"><a href="http://example.com/link2">text2</a></li>
+    <li class="item-0 active"><a href="http://example.com/link3"><b>text3</b></a></li>
+    <li class="item-1 active"><a href="http://example.com/link4">text4</a></li>
+    <li class="item-0">text5</li>
   </ul>
 </div>
 """
@@ -27,12 +27,31 @@ print(doc("p"))
 
 
 ### CSS selector ###
+doc = pq(html)
+items = doc("#container .mylist li").items()
+for item in items:
+  print(item.text())
 
 
 ### Find nodes ###
+print(doc(".mylist").children(".active"))
+print(doc(".mylist").find("li"))
+print(doc("li").parent())
+print(doc("li").parents())
+print(doc(".item-0.active").siblings(".active"))
 
 
-### Traversal ###
+### EXtract information ###
+a = doc(".item-0.active a")
+print(a.attr.href) # == print(a.attr("href"))
+print(a.text())
+print(a.html())
 
 
 ### Nodes operations ###
+li = doc(".item-0.active")
+print(li)
+print(li.remove_class("active"))
+print(li.add_class("myclass"))
+print(li.attr("name", "link"))
+print(li.text("changed text"))
